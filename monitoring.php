3645e1eb0feb7be2bd7883890468b2d11c8edcff
Angga Aditya <90344608+anggadityaa@users.noreply.github.com>
2022-06-11 00:02:26 +0700

    Update monitoring.php

--- monitoring.php
+++ monitoring.php
@@ -1,9 +1,9 @@
-<? php
+<?php
 
 #USERAGENT
-$useragent = "Mozilla/5.0 (Linux; Android 11; SM-M127F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Mobile Safari/537.36";
+$useragent = 'Mozilla/5.0 (Linux; Android 11; SM-M127F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Mobile Safari/537.36' ;
 
 #WALLET
-$wallet = "RMdgw6iSjvjrSU7xRyRnwx5hixUeiwYPEb";
+$wallet = 'RMdgw6iSjvjrSU7xRyRnwx5hixUeiwYPEb';
 
 ?>
